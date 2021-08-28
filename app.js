af0ea75499b454da94b253ec6546160ec5c2b164
debidattasuryaprakash <74270500+debidattasuryaprakash@users.noreply.github.com>
2021-08-28 21:15:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ weather.temperature = {
 // APP CONSTS AND VARS
 const KELVIN = 273;
 // API KEY
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "0dfd384f6562ac5b41465bdda30008aa";
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
